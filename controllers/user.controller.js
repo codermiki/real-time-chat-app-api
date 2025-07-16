3be84767e8e68dfcb66ca481be350d43096b540e
codermiki <mikiastadessemiki@gmail.com>
2025-07-16 13:45:59 +0300

    Fix signup response handling and add signin controller with authentication and token generation

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -9,7 +9,7 @@ export const signup = async (req, res) => {
    try {
       // check all details are filled
       if (!fullName || !email || !password || !bio) {
-         res.json({
+         return res.json({
             success: false,
             message: "Missing Details",
          });
@@ -17,7 +17,7 @@ export const signup = async (req, res) => {
       // check if the email already exists
       const user = await User.findOne({ email });
       if (user) {
-         res.json({
+         return res.json({
             success: false,
             message: "Account already exists",
          });
@@ -39,6 +39,46 @@ export const signup = async (req, res) => {
          success: true,
          message: "Account created successfully",
          data: newUser,
+         token,
+      });
+   } catch (error) {
+      console.log(error.message);
+      res.json({
+         success: false,
+         message: error.message,
+      });
+   }
+};
+
+// Controller to signin a user
+export const signin = async (req, res) => {
+   const { email, password } = req.body;
+
+   try {
+      // check if the email exists
+      const user = await User.findOne({ email });
+      if (!user) {
+         return res.json({
+            success: false,
+            message: "Account does not exist",
+         });
+      }
+
+      // verify password
+      const isValidPassword = await bcrypt.compare(password, user.password);
+      if (!isValidPassword) {
+         return res.json({
+            success: false,
+            message: "Invalid email or password",
+         });
+      }
+
+      // generate token
+      const token = generateToken(user._id);
+      res.json({
+         success: true,
+         message: "Signed in successfully",
+         data: user,
          token,
       });
    } catch (error) {
